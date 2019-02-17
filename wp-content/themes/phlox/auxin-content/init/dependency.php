2a0ea16edf3a81c9d6ade9e001c2d4599d3116cb
nitishpatel <39486276+nitishpatel@users.noreply.github.com>
2019-02-17 12:47:52 +0530

    delete

--- wp-content/themes/phlox/auxin-content/init/dependency.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Dependency Credentials
- */
-if( ! defined('AUXELS_REQUIRED_VERSION') ) define( 'AUXELS_REQUIRED_VERSION', '2.3.2' );
-if( ! defined('AUXPFO_REQUIRED_VERSION') ) define( 'AUXPFO_REQUIRED_VERSION', '1.7.6' );
