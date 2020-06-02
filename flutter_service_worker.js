83ab71d7fec3615096cd896832fcab3a1bc3e1b9
Nitish Patel <nitishp74@gmail.com>
2020-06-03 02:26:25 +0530

    Flutter Web Portfolio in the making

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "7028c51a6775f5d361c898b028c678ff",
 "/": "7028c51a6775f5d361c898b028c678ff",
-"main.dart.js": "54ccc244795655ea78a59d937615e186",
+"main.dart.js": "471ac2e14fde50da7e2e605212bb6241",
 "manifest.json": "4d4b648b2c8e8670823f5c4c7638ab9d"
 };
 
