578948e7ee3d1e346e28bd05ac15c3e3c4028688
Nitish Patel <nitishp74@gmail.com>
2024-01-15 12:49:46 +0530

    Delete unnecessary files and code

--- README.md
+++
@@ -1,1 +0,0 @@
-![PERSONAL SITE](https://i.ibb.co/nr07Pzp/screely-1601754223504.png)
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import Routes from './Routes';
-import "animate.css"
-import { ChakraProvider } from "@chakra-ui/react"
-
-ReactDOM.render(<ChakraProvider><Routes /></ChakraProvider>,document.getElementById('root'));
